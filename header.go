ce5e969e05b32a7f20d95497c38c44f4f0a2eb31
zerosatin121 <164495054+zerosatin121@users.noreply.github.com>
2025-04-15 10:27:22 +0000

    Add files via upload

--- header.go
+++ header.go
@@ -5,9 +5,36 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/fatih/color"
 )
 
+//	func printBanner() {
+//		c := color.New(color.FgCyan).Add(color.Bold)
+//		c.Println(`
+//	  _   _            _               ____
+//	 | | | | ___  _ __| |_ ___ _ __   |  _ \ ___  ___ ___  _ __ ___
+//	 | |_| |/ _ \| '__| __/ _ \ '__|  | |_) / _ \/ __/ _ \| '_ ' _ \
+//	 |  _  | (_) | |  | ||  __/ |     |  _ <  __/ (_| (_) | | | | | |
+//	 |_| |_|\___/|_|   \__\___|_|     |_| \_\___|\___\___/|_| |_| |_|
+//	                          Header Recon Tool - by ZeroSatin121
+//		`)
+//	}
+func printBanner() {
+	cyan := color.New(color.FgCyan).SprintFunc()
+	magenta := color.New(color.FgMagenta).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+	boldWhite := color.New(color.FgWhite, color.Bold).SprintFunc()
+
+	fmt.Println(cyan("  _   _            _               ____                         "))
+	fmt.Println(magenta(" | | | | ___  _ __| |_ ___ _ __   |  _ \\ ___  ___ ___  _ __ ___ "))
+	fmt.Println(green(" | |_| |/ _ \\| '__| __/ _ \\ '__|  | |_) / _ \\/ __/ _ \\| '_ ` _ \\"))
+	fmt.Println(magenta(" |  _  | (_) | |  | ||  __/ |     |  _ <  __/ (_| (_) | | | | | |"))
+	fmt.Println(cyan(" |_| |_|\\___/|_|   \\__\\___|_|     |_| \\_\\___|\\___\\___/|_| |_| |_|"))
+	fmt.Println(boldWhite("\n                  Header Recon Tool - by ZeroSatin121\n"))
+}
 func main() {
+	printBanner()
 	// Define a command-line flag for the URL
 	url := flag.String("url", "", "URL to check headers for")
 	flag.Parse()
